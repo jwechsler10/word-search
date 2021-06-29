9095dba977f960d942c7daaba4c33d633fdb27ef
Jared <jwechsler10@gmail.com>
2021-06-29 15:55:26 -0400

    add CLI args and usage

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 import string
 import random
 import collections
+import sys
+import argparse
 
-"""Returns  """
-def getValidWords():
-    with open('words.txt') as wordsFile:
-        words = wordsFile.read().splitlines()
+def getValidWords(filePath='words.txt'):
+    """Returns a list of valid words read in from a file.
 
-    return words
+    Keyword arguments:
+    filePath -- the path to the file of valid words (default words.txt)
+    """
+    try:
+        with open(filePath) as wordsFile:
+            words = wordsFile.read().splitlines()
+        return words
+    except FileNotFoundError as e:
+        sys.exit('File not found, please check the file path and try again.')
 
 def generateBoard(length, width):
+    """Generate a gameboard based on a user provided length and width.
+
+    Keyword arguments:
+    length -- the length of the board
+    width -- the width of the board
+
+    Returns a 2D array with random lowercase letters to represent the gameboard.
+    """
     return [[random.choice(string.ascii_lowercase) for i in range(length)] for j in range(width)]
 
 def findWordsDown(board, length, width, validWords):
+    """Finds all matching words going down the gameboard.
+
+    Keyword arguments:
+    board -- the gameboard to look for words in
+    length -- the length of the gameboard
+    width -- the width of the gameboard
+    validWords -- a list of valid words to compare against
+    """
     acceptedWords = []
     for j in range(length):
         word = ''
@@ -24,6 +48,14 @@ def findWordsDown(board, length, width, validWords):
 
 
 def findWordsUp(board, length, width, validWords):
+    """Finds all matching words going up the gameboard.
+
+    Keyword arguments:
+    board -- the gameboard to look for words in
+    length -- the length of the gameboard
+    width -- the width of the gameboard
+    validWords -- a list of valid words to compare against
+    """
     acceptedWords = []
     for j in range(length - 1, -1, -1):
         word = ''
@@ -34,6 +66,14 @@ def findWordsUp(board, length, width, validWords):
     return acceptedWords
 
 def findWordsAcross(board, length, width, validWords):
+    """Finds all matching words going right across the gameboard.
+
+    Keyword arguments:
+    board -- the gameboard to look for words in
+    length -- the length of the gameboard
+    width -- the width of the gameboard
+    validWords -- a list of valid words to compare against
+    """
     acceptedWords = []
     for i in range(width):
         word = ''
@@ -44,6 +84,14 @@ def findWordsAcross(board, length, width, validWords):
     return acceptedWords
 
 def findWordsBackwards(board, length, width, validWords):
+    """Finds all matching words going left across the gameboard.
+
+    Keyword arguments:
+    board -- the gameboard to look for words in
+    length -- the length of the gameboard
+    width -- the width of the gameboard
+    validWords -- a list of valid words to compare against
+    """
     acceptedWords = []
     for i in range(width - 1, -1, -1):
         word = ''
@@ -54,6 +102,14 @@ def findWordsBackwards(board, length, width, validWords):
     return acceptedWords
 
 def findDiagonalWords(board, length, width, validWords):
+    """Finds all matching words diagionally on the gameboard.
+
+    Keyword arguments:
+    board -- the gameboard to look for words in
+    length -- the length of the gameboard
+    width -- the width of the gameboard
+    validWords -- a list of valid words to compare against
+    """
     acceptedWords = []
     diagonals = collections.defaultdict(list)
     for i in range(length):
@@ -70,23 +126,32 @@ def findDiagonalWords(board, length, width, validWords):
 
     return acceptedWords
 
-def displayBoard(gameBoard):
-    for row in gameBoard:
+def displayBoard(board):
+    """Prints the gameboard in an easy to read format.
+
+    Keyword arguments:
+    board -- the board to display
+    """
+    for row in board:
         print(row)
 
+
 def main():
+    parser = argparse.ArgumentParser(description='Generates a gameboard of desired length and width and prints any words found within it.')
+    parser.add_argument("-l", "--length", required=True, help='length of gameboard')
+    parser.add_argument("-w", "--width", required=True, help='width of gameboard')
+    parser.add_argument("-sb", "--show-board", action='store_true', required=False, default=False, help='print the gameboard')
+    parser.add_argument("-f", "--file", required=False, default='words.txt', help='path to file of valid words to search for')
+    args = parser.parse_args()
+
     results = []
-    displayBoard = False
+    length = int(args.length)
+    width = int(args.width)
 
-    # get board values from CLI input
-    length = 15
-    width = 15
-
-    validWords = getValidWords()
+    validWords = getValidWords(args.file)
     gameBoard = generateBoard(length,width)
 
-    # if desired display board
-    if displayBoard:
+    if args.show_board:
         displayBoard(gameBoard)
 
     results += findWordsDown(gameBoard, length, width, validWords)
